0d78b39a5ef3ffad82a7d058bb5404a350343872
Cleber-Junior <cleber.san.martins05@gmail.com>
2024-09-18 11:04:22 -0300

    fix: api routes

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,29 +1,36 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import { myApi } from "../api/api";
 
 const Home = () => {
   const [data, setData] = useState([]);
 
-  try {
-    const response = myApi.get("/showAllProject");
-    if (response) {
-      setData(response.data);
-    }
-  } catch (error) {
-    console.error("Erro ao buscar projetos", error);
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await myApi.get("/showAllProject");
+        setData(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar projetos", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Projetos</h1>
       <div style={{ background: "grey", width: "10%" }}>
-        {data.map((project) => (
-          <div key={project.id}>
-            <h2>{project.name}</h2>
-            <p>{project.description}</p>
-          </div>
-        ))}
+        {data.length === 0 ? (
+          <p>Nenhum projeto cadastrado no sistema</p>
+        ) : (
+          data.map((project) => (
+            <div key={project.id}>
+              <h2>{project.name}</h2>
+              <p>{project.description}</p>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
