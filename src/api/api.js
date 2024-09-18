f901c4b7ac7c7ba252a8353d25a1ce65345a7b80
Cleber-Junior <cleber.san.martins05@gmail.com>
2024-09-18 10:38:29 -0300

    feat: show projects

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const myApi = axios.create({
-  baseURL: "http://localhost:8000/api",
-  //clodURL: "",
+  //baseURL: "http://localhost:8000/api",
+  clodURL: "https://newgame-da3be6e96e82.herokuapp.com/",
 });
