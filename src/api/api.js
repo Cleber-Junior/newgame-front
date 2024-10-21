abeee6b0a6d53b450509c8acbc687ca553c0d56f
Cleber-Junior <cleber.san.martins05@gmail.com>
2024-10-20 21:09:26 -0300

    fix: codificação e comunicação com o frontEnd

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const myApi = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
-  // clodURL: "https://newgame-da3be6e96e82.herokuapp.com/api",
+  // baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://newgame-da3be6e96e82.herokuapp.com/api",
 });
