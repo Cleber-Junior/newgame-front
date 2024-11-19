64c283403efc2cf7d6e6bba1a03ec528860cb969
Cleber Junior <cleber.san.martins05@gmail.com>
2024-11-19 16:06:07 -0300

    hotjar

--- src/api/api.js
+++ src/api/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const myApi = axios.create({
   // baseURL: "http://127.0.0.1:8000/api",
-baseURL: "https://newgame-da3be6e96e82.herokuapp.com/api",
+  baseURL: "https://newgame-da3be6e96e82.herokuapp.com/api",
 });
