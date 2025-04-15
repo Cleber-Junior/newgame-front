bac4c50a6375c12f35d98149d23e6152d02c8017
Cleber Junior <cleber.san.martins05@gmail.com>
2025-04-15 15:56:23 -0300

    Fix: remove funciton to save members from Context, move to UtilsUser

--- src/assets/Context/UserContext.jsx
+++ src/assets/Context/UserContext.jsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { myApi } from "../../api/api";
-import { TokenContext } from "./TokenContext";
 
 export const UserContext = React.createContext();
 
 export const UserStorage = ({ children }) => {
-  const { token } = React.useContext(TokenContext);
   const [user, setUser] = useState(() => {
     return JSON.parse(localStorage.getItem("user")) || null;
   });
@@ -23,35 +20,9 @@ export const UserStorage = ({ children }) => {
     }
   }, [user]);
 
-  const handleSave = async (e, userData) => {
-    e.preventDefault();
-    console.log("token", token);
-    console.log("userData", userData);
-
-    try {
-      const response = await myApi.post(`/editUser/${user.id}`, userData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (response.status === 200) {
-          const data = response.data
-          saveUser(data.user);
-          const message = data.msg;
-          const datas = {
-            message: message,
-            user: data.user
-          }
-
-          return datas;
-        }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+  
   return (
-    <UserContext.Provider value={{ saveUser, user, handleSave }}>
+    <UserContext.Provider value={{ saveUser, user }}>
       {children}
     </UserContext.Provider>
   );
